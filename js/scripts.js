3f8150e65794a8d5113848061cd55b79a21ec878
jaafar <jaafarbenabderrazak.info@gmail.com>
2024-09-22 22:16:13 +0200

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+// scripts.js
+
+// Example: Toggle Navigation for Mobile (if needed in future)
+document.addEventListener('DOMContentLoaded', () => {
+    // Future JavaScript functionalities can be added here
+});
