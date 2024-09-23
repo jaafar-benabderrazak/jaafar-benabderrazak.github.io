7b6df9899283d11f934f89904ee6ddca1e73571f
jaafar <jaafarbenabderrazak.info@gmail.com>
2024-09-23 17:35:50 +0200

    better ui/ux

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,5 @@
-// scripts.js
-
 // Example: Toggle Navigation for Mobile (if needed in future)
 document.addEventListener('DOMContentLoaded', () => {
     // Future JavaScript functionalities can be added here
+    // For example: Dark Mode Toggle, Lazy Loading Enhancements, etc.
 });
